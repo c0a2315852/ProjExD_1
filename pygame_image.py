7177f0564f677d0b2eba880e08a4f75b9150a6a9
Kenta Yamaguchi <c0a2315852@edu.teu.ac.jp>
2024-04-16 16:47:20 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,23 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")  #画像を読み込む関数
     img_3 = pg.image.load("fig/3.png") #3.png の読み込み
     img_3 = pg.transform.flip(img_3, True, False) #上下左右反転
+    bg_img_2 = pg.transform.flip(bg_img, True, False) #練習7-1
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr % 3200 #練習6(tmr % 800) 変更後 練習7-2
+        screen.blit(bg_img, [-x, 0]) #練習6
+        screen.blit(bg_img_2, [-x + 1600, 0]) #練習7-1 x座標
+        screen.blit(bg_img, [-x + 3200, 0])  #練習7-2
+        screen.blit(bg_img_2, [-x + 4800, 0]) #練習7-2
+        
+        screen.blit(img_3, [300, 200]) #こうかとんを横300縦200の位置にblit
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1    
+        clock.tick(200) #FPSを200へ
 
 
 if __name__ == "__main__":
